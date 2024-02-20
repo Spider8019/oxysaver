d5b5bcaba03c48a1b945b389b44cf696b707a9a9
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2024-02-20 16:52:09 +0530

    first commit

---
+++ src/pages/HowToUse.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Announcement from '../components/Announcement'
+import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
+
+const HowToUse = () => {
+  return (
+    <>
+      <Navbar />
+      <Announcement />
+      <div className="p-20">
+        <h1 className="text-4xl">How To Use</h1>
+        <div className='grid grid-cols-2 gap-8 my-20'>
+          {[
+            {
+              title: 'Attach',
+              body: 'Open the cup and attach the mask with nozzle',
+              img:
+                'https://i.pinimg.com/564x/58/60/89/586089f3b9fb9d8a255bb924e81477c4.jpg',
+            },
+            {
+              title: 'Inhale',
+              body: 'Put the mask on mouth and take a quick puff',
+              img:
+                'https://i.pinimg.com/564x/58/60/89/586089f3b9fb9d8a255bb924e81477c4.jpg',
+            },
+            {
+              title: 'Exhale',
+              body: 'Exhale without holding the canister',
+              img:
+                'https://i.pinimg.com/564x/58/60/89/586089f3b9fb9d8a255bb924e81477c4.jpg',
+            },
+            {
+              title: 'Repeat',
+              body: 'Take 3 to 5 puffs at a time',
+              img:
+                'https://i.pinimg.com/564x/58/60/89/586089f3b9fb9d8a255bb924e81477c4.jpg',
+            },
+          ].map((item, index) => {
+            return (
+              <div
+                key={index}
+                className=" p-20 rounded-3xl flex gap-8 bg-cyan-600 items-center"
+              >
+                <div>
+                  <h1 className="text-3xl font-semibold">{item.title}</h1>
+                  <p className="text-xl ">{item.body}</p>
+                </div>
+                <img
+                  className="w-40 h-40 rounded-full"
+                  src={item.img}
+                  alt="Adfa"
+                />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <Newsletter />
+      <Footer />
+    </>
+  )
+}
+
+export default HowToUse
