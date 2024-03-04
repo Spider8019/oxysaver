37a42ee3bd9ccf43b2213f43c16fd6d5bc9a8459
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2024-03-04 21:36:23 +0530

    a;skdjfja

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,24 @@ const Home = () => {
       <Announcement />
       <Slider />
       {/* <Categories /> */}
-      <p className='text-3xl sm:text-5xl font-semibold m-4 mt-12'>All Purposes</p>
+      <div className="flex sm:flex-row-reverse items-center justify-around flex-col-reverse m-4 sm:my-20 order-last sm:order-first">
+        <iframe
+          src="https://assets.pinterest.com/ext/embed.html?id=836121487095791101"
+          height="714"
+          width="345"
+          frameborder="0"
+          scrolling="no"
+          title=";ALKJD"
+        ></iframe>
+        <h1 className="font-semibold text-2xl sm:text-8xl py-4">
+          For Better Assistance,
+          <br /> Watch the Video
+        </h1>
+      </div>
+      <p className="text-3xl sm:text-5xl font-semibold m-4 mt-12">
+        All Purposes
+      </p>
+
       <div className="grid sm:grid-cols-2 gap-4 m-4">
         {[
           {
@@ -39,16 +56,25 @@ const Home = () => {
               'https://i.pinimg.com/originals/2d/95/66/2d95662deb38e0c115a96f448bd47856.jpg',
           },
         ].map((item, idx) => {
-          return <div key={idx} className='transition duration-500 bg-white rounded-xl shadow filter items-center grid sm:grid-cols-2 hover:bg-[#66bcd2] overflow-hidden'>
-            <img src={Banner} className='rounded-xl h-full w-full object-cover' alt={`Small banner ${idx}`} />
-            <div className='p-4'>
-              <p className="text-2xl">{item.body}</p>
+          return (
+            <div
+              key={idx}
+              className="transition duration-500 bg-white rounded-xl shadow filter items-center grid sm:grid-cols-2 hover:bg-[#66bcd2] overflow-hidden"
+            >
+              <img
+                src={Banner}
+                className="rounded-xl h-full w-full object-cover"
+                alt={`Small banner ${idx}`}
+              />
+              <div className="p-4">
+                <p className="text-2xl">{item.body}</p>
+              </div>
             </div>
-          </div>
+          )
         })}
       </div>
+
       <Products />
-      <Newsletter />
       <Footer />
     </div>
   )
--- src/pages/HowToUse.jsx
+++ src/pages/HowToUse.jsx
@@ -15,57 +15,74 @@ const HowToUse = () => {
       <Announcement />
       <div className="p-4 sm:p-20">
         <h1 className="text-4xl">How To Use</h1>
-        <div className="grid sm:grid-cols-1 gap-8 my-12 sm:my-20">
-          {[
-            {
-              title: 'Attach',
-              body: 'Open the cup and attach the mask with nozzle',
-              img: howtouse1,
-            },
-            {
-              title: 'Inhale',
-              body: 'Put the mask on mouth and take a quick puff',
-              img: howtouse2,
-            },
-            {
-              title: 'Exhale',
-              body: 'Exhale without holding the canister',
-              img: howtouse3,
-            },
-            {
-              title: 'Repeat',
-              body: 'Take 3 to 5 puffs at a time',
-              img: howtouse4,
-            },
-          ].map((item, index) => {
-            return (
-              <div
-                key={index}
-                className="relative p-8 sm:p-20 rounded-3xl flex flex-col sm:flex-row gap-8 bg-[#66bcd2] sm:items-center sm:justify-between"
-              >
-                <p className="text-xl sm:text-3xl sm:font-bold sm:mb-0 mb-4">
-                  Step {index + 1}
-                </p>
-                <div className="sm:text-center">
-                  <h1 className="text-3xl sm:text-5xl font-semibold sm:my-8">
-                    {' '}
-                    {item.title}
-                  </h1>
-                  <p className="text-xl sm:text-2xl">{item.body}</p>
+        <div className="grid grid-cols-1 sm:grid-cols-[auto,1fr] gap-8">
+          <div className="flex sm:flex-col flex-col-reverse sm:my-20 order-last sm:order-first">
+            <iframe
+              src="https://assets.pinterest.com/ext/embed.html?id=836121487095791101"
+              title="as;ldjkja"
+              className="sm:h-full sm:w-[450px] h-[100vh] w-[100%]"
+              // height="900"
+              // width="450"
+              frameborder="0"
+              scrolling="no"
+            ></iframe>
+            <h1 className="text-4xl py-4">
+              For Better Assistance,
+              <br /> Watch the Video
+            </h1>
+          </div>
+          <div className="grid sm:grid-cols-1 gap-8 my-12 sm:my-20">
+            {[
+              {
+                title: 'Attach',
+                body: 'Open the cup and attach the mask with nozzle',
+                img: howtouse1,
+              },
+              {
+                title: 'Inhale',
+                body: 'Put the mask on mouth and take a quick puff',
+                img: howtouse2,
+              },
+              {
+                title: 'Exhale',
+                body: 'Exhale without holding the canister',
+                img: howtouse3,
+              },
+              {
+                title: 'Repeat',
+                body: 'Take 3 to 5 puffs at a time',
+                img: howtouse4,
+              },
+            ].map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="relative p-8 sm:p-20 rounded-3xl flex flex-col sm:flex-row gap-8 bg-[#66bcd2] sm:items-center sm:justify-between"
+                >
+                  <p className="text-xl sm:text-3xl sm:font-bold sm:mb-0 mb-4">
+                    Step {index + 1}
+                  </p>
+                  <div className="sm:text-center">
+                    <h1 className="text-3xl sm:text-5xl font-semibold sm:my-8">
+                      {' '}
+                      {item.title}
+                    </h1>
+                    <p className="text-xl sm:text-2xl">{item.body}</p>
+                  </div>
+                  <div className="p-4 bg-white rounded-3xl grid place-items-center">
+                    <img
+                      className="w-40 h-40 sm:w-40 sm:h-40"
+                      src={item.img}
+                      alt="Adfa"
+                    />
+                  </div>
                 </div>
-                <div className="p-4 bg-white rounded-3xl grid place-items-center">
-                  <img
-                    className="w-40 h-40 sm:w-40 sm:h-40"
-                    src={item.img}
-                    alt="Adfa"
-                  />
-                </div>
-              </div>
-            )
-          })}
+              )
+            })}
+          </div>
         </div>
       </div>
-      <Newsletter />
+      <div className=""></div>
       <Footer />
     </>
   )
